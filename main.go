3b12e93f2ede118d5a804a35a50afcafffdd993d
Pranav Mishra <mishrapranav82@gmail.com>
2025-06-06 18:20:32 -0400

    web app fixes

--- main.go
+++ main.go
@@ -25,13 +25,33 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/contact.html")
 }
 
-func main() {
+func projectsPage(w http.ResponseWriter, r *http.Request) {
+	// Render the projects html page
+	http.ServeFile(w, r, "static/projects.html")
+}
 
+func main() {
+	// Serve static files like CSS, JS, images
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("static/images"))))
+
+	// Handle page routes
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
+	http.HandleFunc("/projects", projectsPage)
 
+	// Redirect root to home
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.Redirect(w, r, "/home", http.StatusSeeOther)
+		} else {
+			http.NotFound(w, r)
+		}
+	})
+
+	log.Println("Server starting on http://localhost:8080")
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
